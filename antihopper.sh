0c10b052ee6f5c0e96a1040a26ec29ba4c73710c
PNWDrew <13842499+PNWDrew@users.noreply.github.com>
2018-03-20 00:13:33 -0700

    Update antihopper.sh

--- antihopper.sh
+++ antihopper.sh
@@ -1,5 +1,5 @@
 # This runs the payout script but does not send payments. 
-#
+# Do not use if you are using donations% in config. fixed donations are ok.
 # -y              makes it save the results, updating poollogs.json. recording voters, pending, and timestamp. 
 # --min-payout    overrides the value in config.json to stop payouts from being sent
 #
